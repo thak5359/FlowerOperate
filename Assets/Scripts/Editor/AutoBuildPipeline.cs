c6660eff6a6a4fde110d75b18ebb996058c6ff0f
이준복 <soma53597@gmail.com>
2026-03-16 10:00:17 +0900

    Jenkins Setup

---
+++ Assets/Scripts/Editor/AutoBuildPipeline.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+class AutoBuildPipeline
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+}
