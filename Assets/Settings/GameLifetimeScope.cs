08a01522d0f28df7865c72c77e8be533bcfdeeec
Paranjamong <arahan5218@gmail.com>
2026-03-24 12:53:44 +0900

    0324 morning

--- Assets/Settings/GameLifetimeScope.cs
+++ Assets/Settings/GameLifetimeScope.cs
@@ -9,9 +9,6 @@ public class GameLifetimeScope : LifetimeScope
         protected override void Configure(IContainerBuilder builder)
         {
             builder.Register<IAmapManager>(Lifetime.Singleton);
-            builder.Register<IAmapManager>(Lifetime.Singleton);
             builder.Register<PlayerController>(Lifetime.Singleton);
-
-            
         }
     }
